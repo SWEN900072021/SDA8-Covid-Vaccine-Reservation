58e35a3aba1c57079340e69d454797475762bba1
ZhuolunWu-0906 <zhuolunw@student.unimelb.edu.au>
2021-09-16 15:51:32 +1000

    Update postgresqlConnector.java

--- src/src/main/java/com/example/swen90008sda8/postgresqlConnector.java
+++ src/src/main/java/com/example/swen90008sda8/postgresqlConnector.java
@@ -21,9 +21,9 @@ import java.sql.*;
 //}
 
 public class postgresqlConnector {
-    private final String url = "jdbc:postgresql://localhost:5432/myDB";
-    private final String user = "postgres";
-    private final String password = "123456";
+    private final String url = "jdbc:postgresql://ec2-44-198-223-154.compute-1.amazonaws.com/de41193liacu11";
+    private final String user = "afltyojyhsunzv";
+    private final String password = "f353ad528068ff503fe032630811b830cc29a0ac26cca731ae1e43a8ef5575a9";
     /**
      * Connect to the PostgreSQL database
      * @return a Connection object
