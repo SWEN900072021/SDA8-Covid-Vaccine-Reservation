bf4efa97a216eeb04825586f503e9e2b422aad46
PeAt2333 <69140669+PeAt2333@users.noreply.github.com>
2021-10-10 22:16:53 +1100

    fix all bugs

--- src/src/main/java/org/unimelb/cis/swen90007sda8/Servlets/logInServlet.java
+++ src/src/main/java/org/unimelb/cis/swen90007sda8/Servlets/logInServlet.java
@@ -15,7 +15,7 @@ public class logInServlet extends HttpServlet{
 //        response.sendRedirect("index.jsp");
         response.setContentType("text/html");
         Subject currentUser = SecurityUtils.getSubject();
-        userModel user = adminMapper.find(SecurityUtils.getSubject().getPrincipals().toString());
+        userModel user = adminMapper.find(currentUser.getPrincipals().toString());
         SecurityUtils.getSubject().getSession().setAttribute("user", user);
         if(currentUser.hasRole("Admin")){
             response.sendRedirect("adminpage.jsp");
