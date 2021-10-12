629751363201734ecc9f3ab8f2909c68fab8187d
Zhuolun Wu <626084347@qq.com>
2021-10-12 15:14:08 +1100

    Hash users' passwords

--- src/src/main/java/org/unimelb/cis/swen90007sda8/Servlets/signUpProviderServlet.java
+++ src/src/main/java/org/unimelb/cis/swen90007sda8/Servlets/signUpProviderServlet.java
@@ -1,5 +1,11 @@
 package org.unimelb.cis.swen90007sda8.Servlets;
 
+import org.apache.shiro.crypto.hash.DefaultHashService;
+import org.apache.shiro.crypto.hash.Hash;
+import org.apache.shiro.crypto.hash.HashRequest;
+import org.apache.shiro.crypto.hash.SimpleHashRequest;
+import org.apache.shiro.crypto.hash.format.Shiro1CryptFormat;
+import org.apache.shiro.util.ByteSource;
 import org.unimelb.cis.swen90007sda8.Mappers.adminMapper;
 
 import java.io.*;
@@ -22,8 +28,15 @@ public class signUpProviderServlet extends HttpServlet{
         String hcpname = request.getParameter("hcpName");
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
+
+        HashRequest hashRequest = new SimpleHashRequest("SHA-256", ByteSource.Util.bytes(pass), ByteSource.Util.bytes(user), 500000);
+        DefaultHashService hashService = new DefaultHashService();
+        Hash hash = hashService.computeHash(hashRequest);
+        Shiro1CryptFormat format = new Shiro1CryptFormat();
+        String passHashed = format.format(hash);
+
         try {
-            if(adminMapper.insertNewProvider(user,pass,identity,post,top,hcpname)){
+            if(adminMapper.insertNewProvider(user,passHashed,identity,post,top,hcpname)){
                 writer.println("<h3>Registion Complete!<br>" +
                         "<a href=\"adminpage.jsp\">Go Back<a>");
             }else{
