9e3ca7c5b63f31e3830bae1bde60adb2e1484d81
PeAt2333 <69140669+PeAt2333@users.noreply.github.com>
2021-10-15 14:21:17 +1100

    update locks

--- src/src/main/java/org/unimelb/cis/swen90007sda8/Mappers/recipientMapper.java
+++ src/src/main/java/org/unimelb/cis/swen90007sda8/Mappers/recipientMapper.java
@@ -1,6 +1,7 @@
 package org.unimelb.cis.swen90007sda8.Mappers;
 
 import org.unimelb.cis.swen90007sda8.DBConnector.postgresqlConnector;
+import org.unimelb.cis.swen90007sda8.LockManager.lockManager;
 
 import java.sql.ResultSet;
 import java.util.Dictionary;
@@ -9,8 +10,10 @@ import java.util.Hashtable;
 public class recipientMapper implements UserInterface {
     public static Boolean getVaccinatedByEmail(String email){
         Boolean result = null;
+        lockManager.getInstance().acquireLock("user "+email, Thread.currentThread().getName());
         String s = "SELECT vaccinated FROM users WHERE email = '" + email + "';";
         ResultSet rs = postgresqlConnector.getInstance().connect(s);
+        lockManager.getInstance().releaseLock("user "+email, Thread.currentThread().getName());
         try {
             if (rs.next()) {
                 result = rs.getBoolean(1);
@@ -22,9 +25,11 @@ public class recipientMapper implements UserInterface {
     }
 
     public static Dictionary<Object, Object> findUserByEmail(String email){
+        lockManager.getInstance().acquireLock("user "+email, Thread.currentThread().getName());
         String stmt = "SELECT email, dateofbirth, firstname, lastname, user_identity, postcode, typeofprovider, vaccinated, hcpname FROM users " +
                 "WHERE email = '"+email+"';";
         ResultSet rs = postgresqlConnector.getInstance().connect(stmt);
+        lockManager.getInstance().releaseLock("user "+email, Thread.currentThread().getName());
         Dictionary<Object, Object> user = new Hashtable<>();
         try {
             while (rs.next()) {
